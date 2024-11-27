b62028b4d1d4e2e3f60a1cfa51e0211580dd32ca
mahfujarr <mahfujarrahmanjoy@gmail.com>
2024-11-27 16:47:02 +0600

    complete 1

--- Lab6/1.cpp
+++ Lab6/1.cpp
@@ -1,2 +1,36 @@
 #include <iostream>
 using namespace std;
+class ParkingSlot
+{
+public:
+    int slotNumber;
+    bool isOccupied;
+    ParkingSlot()
+    {
+        isOccupied = false;
+    }
+    void occupySlot()
+    {
+        isOccupied = true;
+    }
+    void displayStatus()
+    {
+        cout << "Slot Number: " << slotNumber << endl;
+        cout << "Occupied: " << (isOccupied ? "Yes" : "No") << endl;
+    }
+};
+int main()
+{
+    ParkingSlot slot1, slot2;
+    slot1.slotNumber = 1;
+    slot2.slotNumber = 2;
+
+    slot1.displayStatus();
+    slot2.displayStatus();
+
+    slot1.occupySlot();
+
+    slot1.displayStatus();
+    slot2.displayStatus();
+    return 0;
+}
