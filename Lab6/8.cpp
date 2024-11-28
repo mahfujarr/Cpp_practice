b9d444d26653c7ac14b125668a6e8daa9d2b32cd
mahfujarr <mahfujarrahmanjoy@gmail.com>
2024-11-29 01:19:04 +0600

    edit 8

--- Lab6/8.cpp
+++ Lab6/8.cpp
@@ -1,1 +1,21 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+class Event
+{
+private:
+    int eventID;
+    string eventName;
+    string eventDate;
+    bool isScheduled;
+public:
+    Event(string name, int ID = 000, string date = "Null")
+    {
+        eventName = name;
+        eventID = ID;
+        eventDate = date;
+    }
+    void scheduled(bool isScheduled)
+    {
+        isScheduled = !isScheduled;
+    }
+};
